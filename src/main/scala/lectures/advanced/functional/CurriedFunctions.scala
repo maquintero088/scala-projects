1e76c92e3b9e145cfeed95963fd01535518a179c
mequintero <mequintero@personalsoft.com.co>
2024-05-28 07:53:32 -0500

    Advanced Functional Programming update
    Scala objects lectures

--- src/main/scala/lectures/advanced/functional/CurriedFunctions.scala
+++ src/main/scala/lectures/advanced/functional/CurriedFunctions.scala
@@ -2,4 +2,41 @@ package lectures.advanced.functional
 
 object CurriedFunctions extends App {
 
+  // curried functions
+  val superAdder: Int => Int => Int =
+    x => y => x + y
+
+  val add3 = superAdder(3) 
+  // Int => Int = y => 3 + y
+  println(add3(5))
+  println(superAdder(3)(5)) 
+  // curried function
+
+  // METHOD!
+  def curriedAdder(x: Int)(y: Int): Int = x + y
+  // curried method
+
+  val add4: Int => Int = curriedAdder(4)
+  // lifting = ETA-EXPANSION
+
+  // functions != methods (JVM limitation)
+  def inc(x: Int) = x + 1
+  List(1,2,3).map(x => inc(x)) 
+  // ETA-expansion
+  // EXERCISES
+  /*
+    1.  Process a list of numbers and return their string representations with different formats
+        Use the %4.2f, %8.6f and %14.12f with a curried formatter function.
+   */
+  def curriedFormatter(s: String)(number: Double): String = s.format(number)
+
+  val numbers = List(Math.PI, Math.E, 1, 9.8, 1.3e-12)
+
+  val simpleFormat = curriedFormatter("%4.2f") _
+  // lift
+  val seriousFormat = curriedFormatter("%8.6f") _
+  val preciseFormat = curriedFormatter("%14.12f") _
+
+  println(numbers.map(curriedFormatter("%14.12f")))
+  // compiler does sweet eta-expansion for us
 }
