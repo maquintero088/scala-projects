465568f5a8bb034560189e2f50debb1ec33165b1
mequintero <mequintero@personalsoft.com.co>
2024-05-28 11:54:31 -0500

    Implicits update
    Scala objects lectures

--- src/main/scala/lectures/implicits/Givens.scala
+++ src/main/scala/lectures/implicits/Givens.scala
@@ -1,5 +1,25 @@
 package lectures.implicits
 
 object Givens extends App {
+  val aList = List(2,4,3,1)
+  val anOrderedList = aList.sorted // implicit Ordering[Int]
+
+  // Scala 2 style
+  object Implicits {
+    implicit val descendingOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _)
+  }
+
+  // Scala 3 style
+  object Givens {
+    given descendingOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _)
+    // givens <=> implicit vals
+  }
+
+  // instantiating an anonymous class
+  object GivenAnonymousClassNaive {
+    given descendingOrdering_v2: Ordering[Int] = new Ordering[Int] {
+      override def compare(x: Int, y: Int) = y - x
+    }
+  }
 
 }
